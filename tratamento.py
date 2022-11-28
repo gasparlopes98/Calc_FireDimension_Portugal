2858519a7a61660bf53b848d12774003f0d927b3
Goncalo Lopes <gasparlopes98gmail.com>
2022-11-28 15:56:30 +0000

    messing around

--- tratamento.py
+++ tratamento.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
 
@@ -28,17 +29,56 @@ df.pop('thumbnails.high.url')
 
 # Creating new Column
 df['contrast'] = 0
+df['hue'] = 0
+df['saturation'] = 0
+df['brightness'] = 0
 
+######################
+# Cut Black Borders
+######################
 # for i in range(len(df)):
 #     img = cv2.imread('cocomelon/thumbnails/{}.jpg'.format(df.loc[i, 'id']),3)
 #     crop_img = img[45:315,:] # Take off Black Borders
 #     cv2.imwrite('cocomelon/thumbnails_ready/{}.jpg'.format(df.loc[i, 'id']), crop_img)
 
-# Adding values to contraste
+# Adding values to contrast
 for i in range(len(df)):
     img = cv2.imread('cocomelon/thumbnails_ready/{}.jpg'.format(df.loc[i, 'id']),0)
-    df.loc[i, 'contrast'] = img.std()
+    df.loc[i, 'contrast'] = round(img.std(),3)
+    
+# Adding values to saturation
+for i in range(len(df)):
+    img = cv2.imread('cocomelon/thumbnails_ready/{}.jpg'.format(df.loc[i, 'id']))
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    df.loc[i, 'saturation'] = round(img_hsv[:, :, 1].mean())
+    
+######################
+# Dominant Colors
+######################
+# dominant_colors=[]
+# # Adding values to dominant_color
+# for i in range(len(df)):
+#     img = cv2.imread('cocomelon/thumbnails_ready/{}.jpg'.format(df.loc[i, 'id']))
+#     height, width, _ = np.shape(img)
 
+#     data = np.reshape(img, (height * width, 3))
+#     data = np.float32(data)
+
+#     number_clusters = 3
+#     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+#     flags = cv2.KMEANS_RANDOM_CENTERS
+#     _, _, centers = cv2.kmeans(data, number_clusters, None, criteria, 10, flags)
+#     dominant_colors.append(centers.astype(int))
+    
+# print(dominant_colors)
+# # with open('cocomelon/dominant_color.npy', 'wb') as f:
+# #     np.save(f, dominant_colors)
+
+# Adding values to hue  
+for i in range(len(df)):
+    img = cv2.imread('cocomelon/thumbnails_ready/{}.jpg'.format(df.loc[i, 'id']),cv2.COLOR_RGB2HSV) # open as HSV color space
+    df.loc[i, 'hue'] = round(np.mean(img[0]))
+ 
 # Saving new data base
 df.to_csv('cocomelon/cocomelon_stats_ready.csv')
 print(df.head(5))
