968bc105110b3b71579ac9652e791c65cd13ef43
Mevlor <joao_azevedo10@hotmail.com>
2022-12-07 18:39:53 +0000

    Dados com novas alteracoes

--- TratamentoDados.py
+++ TratamentoDados.py
@@ -4,10 +4,10 @@ from datetime import datetime
 import cv2
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("datasets/datasetDadosTratados.csv")
-print(df.columns)
+#df = pd.read_csv("datasets/DadosConcatenados.csv")
+#print(df.columns)
 
-# Removing irrelevant data (Only for the first time!)
+# Removing irrelevant data and  (Only for the first time!)
 '''
 df.pop('Codigo_SGIF')
 df.pop('Codigo_ANEPC')
@@ -22,14 +22,14 @@ df.pop('X_Militar')
 df.pop('Y_Militar')
 df.pop('X_ETRS89')
 df.pop('Y_ETRS89')
-df.pop('TipoCausa')
-df.pop('GrupoCausa')
-df.pop('DescricaoCausa')
-df.pop('FonteAlerta')
-'''
+#df.pop('TipoCausa')
+#df.pop('GrupoCausa')
+#df.pop('DescricaoCausa')
+#df.pop('FonteAlerta')
+
 
 #Criacao TempoAlertaIntervencao
-'''
+
 df['tempoAlertaIntervencao'] = 0
 
 for i in range(len(df)):
@@ -42,13 +42,8 @@ for i in range(len(df)):
     except:
         pass
 
-# Saving new data base
-df.to_csv('datasetDadosTratados.csv')
-'''
-#df['tempoAlertaIntervencao'] = 0
-
 #Definição das Classes!
-'''
+
 for i in range(len(df)):
     if df.loc[i, 'ClasseArea'] == "]0 a 1 ha[":
        df.loc[i, 'ClasseArea'] = 0
@@ -73,10 +68,36 @@ for i in range(len(df)):
 
     elif df.loc[i, 'ClasseArea'] == "[superior a 1000 ha]":
         df.loc[i, 'ClasseArea'] = 7
+'''
 
+
+df = pd.read_csv("datasets/datasetDadosTratados.csv")
+#print(df.columns)
+
+#ID para primeira coluna
+'''
+df.columns.values[0] = "id"
+res = df.columns.values[0]
+# displaying column
+print("Displaying column names : ",res)
 '''
 
 #Saving new data base
+j = 0
+aux = 0
+for i in range(len(df)):
+    if df.loc[i, 'AreaTotal_ha'] < 0.01:
+        aux += 1
+        print("DMC ->" + str(df.loc[i,'DMC']))
+
+    if df.loc[i, 'AreaTotal_ha'] < 0.01 and df.loc[i, 'TipoCausa'] == "Desconhecida":
+        j+= 1
+        #print(df.loc[i, 'AreaTotal_ha'])
+        #print(df.loc[i, 'TipoCausa'])
+
+print(j)
+print(aux)
+
 #df.to_csv('datasets/datasetDadosTratados.csv', index = False)
 
 
