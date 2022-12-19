82823f13a904669e018243df6d024d672fd0dcae
Guilherme-Mendes-ISEP <1190641@isep.ipp.pt>
2022-12-19 20:38:42 +0000

    Mapa mais bonito

--- website/fire-website/src/App.js
+++ website/fire-website/src/App.js
@@ -28,7 +28,7 @@ function App() {
 
       <TileLayer
         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
       />
     </MapContainer>
   );
